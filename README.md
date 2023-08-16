a96359859fa2b1d0529b83b2e5900984d4423c93
Ekansh Goel <ekanshgoel123@gmail.com>
2023-08-16 19:30:47 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Spark_of_Motivation
- Random motivation prompt 
+ Random motivation prompt
+
+ 16 august 2023
+ Initial commit.
+
+ The basic setup is done.
+ prompts are coming in console.log.
+
+
+ pending tasks
+
+ Adding button function
+ Copy to clip board.
+
---
+++ logic.js
@@ -0,0 +1,20 @@
+let arr = [
+  "Embrace the challenges, for they mold your strength.",
+  "Strive for progress, not perfection.",
+  "Every step forward is a victory in itself.",
+  "Believe in your journey, even if you can't see the destination.",
+  "The only limit is the one you set for yourself.",
+  "Dream big, work hard, and make it happen.",
+  "You have the power to turn dreams into reality.",
+  "Success is the sum of small efforts repeated day in and day out.",
+  "Difficult roads often lead to beautiful destinations.",
+  "Your time is now; make it count.",
+  "Stay focused and never lose sight of your goals.",
+  "Rise after every fall with greater determination.",
+  "Chase your passions relentlessly; they know the way.",
+  "In the midst of challenges, lies opportunity.",
+  "Your attitude determines your direction.",
+];
+// let sum=document.getElementById("generate motivation");
+let num = Math.floor(Math.random() * (arr.length + 1));
+console.log(arr[num]);
